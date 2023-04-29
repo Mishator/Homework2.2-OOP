f36755101a6df343f76251e8c3fc61c8e8a59d6a
Mishator <voyteh89@mail.ru>
2023-04-29 21:05:44 +0300

    Реализован класс hogwarts с описанием свойств, присущих всем ученикам

---
+++ src/Hogwarts.java
@@ -0,0 +1,36 @@
+public class Hogwarts {
+    private String nameStudent;
+    private int witchcraft;
+    private int transgression;
+
+    public String getNameStudent() {
+        return nameStudent;
+    }
+
+    public void setNameStudent(String nameStudent) {
+        this.nameStudent = nameStudent;
+    }
+
+    public int getWitchcraft() {
+        return witchcraft;
+    }
+
+    public void setWitchcraft(int witchcraft) {
+        this.witchcraft = witchcraft;
+    }
+
+    public int getTransgression() {
+        return transgression;
+    }
+
+    public void setTransgression(int transgression) {
+        this.transgression = transgression;
+    }
+
+    public Hogwarts(String nameStudent, int witchcraft, int transgression) {
+        this.nameStudent = nameStudent;
+        this.witchcraft = witchcraft;
+        this.transgression = transgression;
+    }
+}
+
